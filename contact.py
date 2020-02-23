d45daaf8666af4b01c8d21bce135a3e583e341b1
raeman21 <mandelar94@gmail.com>
2020-02-23 20:48:43 +0300

    create class variable contact list

--- contact.py
+++ contact.py
@@ -3,6 +3,8 @@ class Contact:
     Class that generates new instances of contacts
     """
 
+    contact_list = [] #Empty contact list
+
     def __init__(self, firs_name, last_name, phone_number, email):
        
         self.first_name = first_name
