f564bdca28564d8bc37b932f7793ce18789750cd
raeman21 <mandelar94@gmail.com>
2020-02-23 21:09:53 +0300

    firstname typo fixed error not defined

--- contact.py
+++ contact.py
@@ -3,10 +3,10 @@ class Contact:
     Class that generates new instances of contacts
     """
 
-    contact_list = [] #Empty contact list
+    contact_list = []  # Empty contact list
 
-    def __init__(self, firs_name, last_name, phone_number, email):
-       
+    def __init__(self, first_name, last_name, phone_number, email):
+
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
