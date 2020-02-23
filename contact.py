eda59395b228c961d5dda484359ce5f142747ac4
raeman21 <mandelar94@gmail.com>
2020-02-23 20:45:03 +0300

    docstring removed for simplicity

--- contact.py
+++ contact.py
@@ -2,16 +2,10 @@ class Contact:
     """
     Class that generates new instances of contacts
     """
-    def __init__(self,firs_name,last_name,phone_number,email):
 
-        '''
-        __init__method that helps us define properties for our objects.
-
-        Args:
-            first_name:New contact first name.
-            last_name: New contact last name.
-            number: New contact phone number.
-            email: New contact email address.
-
-        '''
-    
+    def __init__(self, firs_name, last_name, phone_number, email):
+       
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
