95c5258986352e6daa66c28dbb3d00a556a090d3
raeman21 <mandelar94@gmail.com>
2020-02-23 20:43:01 +0300

    constructor lesson of self variable+parameters

---
+++ contact.py
@@ -0,0 +1,17 @@
+class Contact:
+    """
+    Class that generates new instances of contacts
+    """
+    def __init__(self,firs_name,last_name,phone_number,email):
+
+        '''
+        __init__method that helps us define properties for our objects.
+
+        Args:
+            first_name:New contact first name.
+            last_name: New contact last name.
+            number: New contact phone number.
+            email: New contact email address.
+
+        '''
+    
